9474f066edcac820acedd1af1125d777abc8f50e
Josua Krause <josua.krause@gmail.com>
2023-07-03 15:11:09 -0400

    expire access tokens

--- routes/helpers/datastructures/index.js
+++ routes/helpers/datastructures/index.js
@@ -158,7 +158,7 @@ exports.pagemetadata = (_kwargs) => {
 				apps_in_suite: apps_in_suite.map((d) => {
 					if (uuid) {
 						const curHost = `${d.baseurl}`.replace(/^https?:\/\//, '').replace(/\/.*$/, '');
-						const token = jwt.sign({ uuid, rights }, process.env.APP_SECRET, { audience: 'user:known', issuer: curHost })
+						const token = jwt.sign({ uuid, rights }, process.env.APP_SECRET, { audience: 'user:known', issuer: curHost, expiresIn: '1h' })
 						d.forwardURL = `${d.baseurl.replace(/\/.*$/, '')}/transfer?path=%2F&token=${token}`;
 					} else {
 						d.forwardURL = d.baseurl;
