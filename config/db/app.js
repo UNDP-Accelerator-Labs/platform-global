0ec9e43c4fd7a2c660961c4d03b4d023fc4991aa
Josua Krause <josua.krause@gmail.com>
2023-07-03 13:47:04 -0400

    add tokens for links

--- config/db/app.js
+++ config/db/app.js
@@ -7,8 +7,8 @@ if (['production', 'local-production'].includes(process.env.NODE_ENV)) {
 			key: d.key,
 			baseurl: d.baseurl,
 			conn: {
-				database: d.key, 
-				port: process.env.DB_PORT, 
+				database: d.key,
+				port: process.env.DB_PORT,
 				host: process.env.DB_HOST,
 				user: process.env.DB_USERNAME,
 				password: process.env.DB_PASSWORD,
@@ -16,34 +16,19 @@ if (['production', 'local-production'].includes(process.env.NODE_ENV)) {
 			}
 		}
 	})
-	exports.connection = {
-		database: process.env.DB_NAME, 
-		port: process.env.DB_PORT, 
-		host: process.env.DB_HOST,
-		user: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		ssl: true
-	}
 } else {
 	console.log('in local test envorinment')
 	exports.connections = apps_in_suite.map(d => {
-		return { 
+		return {
 			key: d.key,
 			baseurl: d.baseurl,
 			conn: {
-				database: d.key, 
-				port: process.env.port, 
+				database: d.key,
+				port: process.env.port,
 				host: process.env.host,
 				user: process.env.user,
 				password: process.env.password
 			}
 		}
 	})
-	exports.connection = {
-		database: process.env.database, 
-		port: process.env.port, 
-		host: process.env.host,
-		user: process.env.user,
-		password: process.env.password
-	}
 }
