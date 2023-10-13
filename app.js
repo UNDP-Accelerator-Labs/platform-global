2663a8fb15c2ed24e227b22a938f509508da97f2
Adedapo Aderemi <adedapopaul@yahoo.com>
2023-10-13 13:51:18 +0100

    update csp frame-src options

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(
         'script-src-attr': ["'unsafe-inline'"],
         'style-src': csp_links,
         'connect-src': csp_links,
-		'frame-src': ['https://youtube.com']
+		'frame-src': ["https://www.youtube.com/","https://youtube.com/","https://web.microsoftstream.com"]
       },
     },
     referrerPolicy: {
