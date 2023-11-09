19baa0d86733168bd67646dd5a41fac5371cab23
Josua Krause <josua.krause@gmail.com>
2023-11-09 10:42:00 -0500

    define jwt

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const express = require('express');
 const path = require('path');
 const bodyparser = require('body-parser');
 const session = require('express-session');
+const jwt = require('jsonwebtoken');
 const PgSession = require('connect-pg-simple')(session);
 
 const fs = require('fs');
